be5eacfcf68f7fadaef409e07463c092e8bd74e2
Jozef Harinek <j.harinek@gmail.com>
2018-11-12 10:15:43 +0100

    Add book model

---
+++ src/models/Book.ts
@@ -0,0 +1,11 @@
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Book extends BaseEntity {
+  @PrimaryGeneratedColumn() id: number;
+
+  @Column() name: string;
+  @Column() description: string;
+  @CreateDateColumn() createdAt: Date;
+  @UpdateDateColumn() updatedAt: Date;
+}

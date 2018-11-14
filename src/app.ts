2fe0a2c7cd0abca4cd26cd5112fb97734ae48545
Jozef Harinek <j.harinek@gmail.com>
2018-11-14 13:06:56 +0100

    Add middleware that catches errors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import "reflect-metadata";
-import express from "express";
+import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import { authorsRoutes } from "./routes/AuthorsRoutes";
 import { booksRoutes } from "./routes/BooksRoutes";
+import { ModelValidationError } from "./errors/ModelValidationError";
 
 class App {
   public app: express.Application;
@@ -17,6 +18,27 @@ class App {
     this.app.use(bodyParser.urlencoded({ extended: true }));
     this.app.use("/api/authors", authorsRoutes);
     this.app.use("/api/books", booksRoutes);
+    
+    this.app.use((err: ModelValidationError, req: Request, res: Response, next: Function) => {
+      if(err instanceof ModelValidationError){
+        res.status(200).send({
+          data: null,
+          errors: err.errorMessages()
+        });
+      } else{
+        next(err);
+      }
+      
+    });
+
+    this.app.use((err: Error, req: Request, res: Response, next: Function) => {
+      console.log(err.stack);
+
+      res.status(500).send({
+        data: null,
+        errors: ["Server encountered an error."]
+      });
+    });
   }
 }
 
